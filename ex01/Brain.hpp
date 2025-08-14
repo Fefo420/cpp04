ab219ed39b875928823b97235449f4efcaee6841
Fefo420 <fileonar@student.42Heilbronn.de>
2025-08-14 18:27:48 +0200

    Brain Main And Make

---
+++ ex01/Brain.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Brain.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fefo <fefo@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/13 13:35:01 by fefo              #+#    #+#             */
+/*   Updated: 2025/08/14 18:03:41 by fefo             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BRAIN_HPP
+# define BRAIN_HPP
+
+# include "Cat.hpp"
+# include "Dog.hpp"
+# include "Animal.hpp"
+
+
+class Brain
+{
+    protected:
+        std::string ideas[100];
+    public:
+        Brain();
+        Brain(const Brain& other);
+        Brain& operator=(const Brain& other);
+        ~Brain();
+        std::string getIdea(int i) const { return ideas[i]; }
+        void setIdea(int i, const std::string& idea) { ideas[i] = idea; }
+};
+
+#endif
